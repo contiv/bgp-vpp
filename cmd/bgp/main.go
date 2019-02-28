f4fecd0a3a83cc7101e95c699da4024e66712a4f
Barbara Moretto <bmorettodama@gmail.com>
2019-02-28 10:14:34 -0800

    attempting to install dep

--- cmd/bgp/main.go
+++ cmd/bgp/main.go
@@ -1,32 +1,39 @@
 package main
 
 import (
-	"github.com/contiv/vpp/plugins/contivconf"
-	"github.com/contiv/vpp/plugins/controller"
-	"github.com/contiv/vpp/plugins/ipam"
-	"github.com/contiv/vpp/plugins/ipv4net"
-	"github.com/contiv/vpp/plugins/nodesync"
-	"github.com/contiv/vpp/plugins/podmanager"
-	"github.com/contiv/vpp/plugins/policy"
-	"github.com/contiv/vpp/plugins/service"
-	"github.com/contiv/vpp/plugins/statscollector"
-	"os"
-
 	"github.com/ligato/cn-infra/agent"
-
-	//plugins that will need to be included in our app
-	"github.com/ligato/cn-infra/logging/logmanager"
-	"github.com/ligato/vpp-agent/plugins/orchestrator"
-	"github.com/ligato/vpp-agent/plugins/kvscheduler"
-	"github.com/ligato/vpp-agent/vendor/github.com/ligato/cn-infra/datasync/kvdbsync"
-	"github.com/ligato/cn-infra/datasync"
-	"github.com/ligato/cn-infra/rpc/rest"
 )
+
+
+func main() {
+	// Create an instance of our plugin using its constructor.
+	p := NewBgpPlugin()
+
+	// Create new agent with our plugin instance.
+	a := agent.NewAgent(agent.Plugins(p))
+
+	// Run starts the agent with plugins, wait until shutdown
+	// and then stops the agent and its plugins.
+	if err := a.Run(); err != nil {
+		logging.DefaultLogger.Error(err)
+	}
+}
+
+
 
 // BgpAgent manages vswitch in contiv/vpp solution
 type BgpAgent struct {
-	LogManager  *logmanager.Plugin
+	LogManager   *logmanager.Plugin
 	Orchestrator *orchestrator.Plugin
-	KVScheduler *kvscheduler.Scheduler
-	REST      *rest.Plugin
+	KVScheduler  *kvscheduler.Scheduler
+	Rest         *rest.Plugin
+}
+
+// New creates new OsseusAgent instance.
+func New() *BgpAgent {
+
+	return &BgpAgent{
+
+	}
+
 }
